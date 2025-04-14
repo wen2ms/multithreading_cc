8e481eea52cc664148503963098432485d73cd1d
wen2ms <1819509111@qq.com>
2025-04-14 14:42:54 +0800

    Feat: timed_mutex

--- mutex.cpp
+++ mutex.cpp
@@ -33,15 +33,36 @@ class Operation {
         }
     }
 
+    void work() {
+        while (true) {
+            if (timed_mutex_.try_lock_for(std::chrono::milliseconds(100))) {
+                count_++;
+                std::this_thread::sleep_for(std::chrono::milliseconds(500));
+                std::cout << "Grabbed the mutex, thread ID: " << std::this_thread::get_id() << std::endl;
+                timed_mutex_.unlock();
+                break;
+            } else {
+                std::cout << "Not grabbed the mutex, thread ID: " << std::this_thread::get_id() << std::endl;
+                std::this_thread::sleep_for(std::chrono::milliseconds(50));
+            }
+        }
+    }
+
   private:
     int number_ = 999;
     std::recursive_mutex mutex_;
+
+    int count_ = 0;
+    std::timed_mutex timed_mutex_;
 };
 
 int main() {
     Operation operation;
-    std::thread t1(&Operation::increment, &operation, 10);
-    std::thread t2(&Operation::decrement, &operation, 10);
+    // std::thread t1(&Operation::increment, &operation, 10);
+    // std::thread t2(&Operation::decrement, &operation, 10);
+
+    std::thread t1(&Operation::work, &operation);
+    std::thread t2(&Operation::work, &operation);
 
     t1.join();
     t2.join();
