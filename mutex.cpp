e5ed780925c09da1a6739ad281bed41e5d4169eb
wen2ms <1819509111@qq.com>
2025-04-14 14:07:48 +0800

    Feat: mutex

---
+++ mutex.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+class Operation {
+  public:
+    void increment(int count) {
+        for (int i = 0; i < count; ++i) {
+            mutex_.lock();
+            ++number_;
+            std::cout << "++current number: " << number_ << std::endl;
+            mutex_.unlock();
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        }
+    }
+
+    void decrement(int count) {
+        for (int i = 0; i < count; ++i) {
+            mutex_.lock();
+            --number_;
+            std::cout << "--current number: " << number_ << std::endl;
+            mutex_.unlock();
+
+            std::this_thread::yield();
+        }
+    }
+
+  private:
+    int number_ = 999;
+    std::mutex mutex_;
+};
+
+int main() {
+    Operation operation;
+    std::thread t1(&Operation::increment, &operation, 10);
+    std::thread t2(&Operation::decrement, &operation, 10);
+
+    t1.join();
+    t2.join();
+
+    return 0;
+}
